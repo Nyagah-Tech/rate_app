d90330e85f0ad877cfc7f2dfc64e5c459c9c2edb
dan <dan@localhost.localdomain>
2020-01-10 15:37:21 +0300

    the registration functionality

--- rate_app/views.py
+++ rate_app/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+
+
+
+@login_required(login_url='/accounts/login/')
+def home(request):
+    return render(request,"all/index.html")
 
 
 # Create your views here.
