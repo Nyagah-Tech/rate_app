4deb4f9aa693426233838b821cd37f36e77c404e
dan <dan@localhost.localdomain>
2020-01-10 21:49:30 +0300

    create the login functionality

--- rate_app/models.py
+++ rate_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete =models.CASCADE)
+    profile_pic = models.ImageField(upload_to="/profile",default = default.jpg)
+    bio = HTMLField()
+    updated_on = models.DateTimeField(auto_now_add=True)
 
 # Create your models here.
