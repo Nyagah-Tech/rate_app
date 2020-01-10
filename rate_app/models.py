a8b09de86ade395df4fe73b25d59771645ffe4b0
dan <dan@localhost.localdomain>
2020-01-10 23:26:40 +0300

    create a  default  value for our image columns

--- rate_app/models.py
+++ rate_app/models.py
@@ -5,17 +5,17 @@ from tinymce.models import HTMLField
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete =models.CASCADE)
-    profile_pic = models.ImageField(upload_to="/profile",default = default.jpg)
+    profile_pic = models.ImageField(upload_to="profile/",default = "default.jpg")
     bio = HTMLField()
     updated_on = models.DateTimeField(auto_now_add=True)
 
 class Project_Post(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE)
-    img_project = models.ImageField(upload_to="/project")
+    img_project = models.ImageField(upload_to="project/")
     posted_on = models.DateTimeField(auto_now_add=True)
 
 class Reviews(models.Model):
-    review = models.HTMLField()
+    review = HTMLField()
     posted_by = models.ForeignKey(User,on_delete=models.CASCADE)
     posted_on = models.DateTimeField(auto_now_add=True)
     project_id = models.ForeignKey(Project_Post,on_delete=models.CASCADE)
