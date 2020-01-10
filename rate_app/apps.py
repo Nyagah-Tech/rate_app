07afe430f693883a56b5334977f247203bbcbf27
dan <dan@localhost.localdomain>
2020-01-10 22:13:32 +0300

    register our applicaton at the setting.py as well connect it iwth postgres bd sql

--- rate_app/apps.py
+++ rate_app/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class RateAppConfig(AppConfig):
     name = 'rate_app'
+    
+    def ready(self):
+        import signals
--- rates/settings.py
+++ rates/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tinymce',
     'bootstrap',
+    'rate_app.apps.RateAppConfig',
     
 ]
 
@@ -78,8 +79,11 @@ WSGI_APPLICATION = 'rates.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER':config('DB_USER'),
+        'PASSWORD':config('DB_PASSWORD'),
+        'PORT': '',
     }
 }
 
@@ -121,3 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
