abd2c616274c110df4e94a825268cf36432f7190
dan <dan@localhost.localdomain>
2020-01-11 12:35:14 +0300

    create the urls/path that links the post project form with the  view function

--- rate_app/urls.py
+++ rate_app/urls.py
@@ -5,8 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     path('',views.home,name="home"),
+    path('new/project/',views.post_project_view,name="post-project"),
+    
 ]
 
-]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
