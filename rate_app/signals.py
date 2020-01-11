f16645ec47ec664e0f453f4af9e26585081bd8b7
dan <dan@localhost.localdomain>
2020-01-11 13:41:50 +0300

    fix a an error at the signals.py file

--- rate_app/signals.py
+++ rate_app/signals.py
@@ -6,8 +6,8 @@ from .models import Profile
 @receiver(post_save, sender = User)
 def create_profile(sender,instance,created,**kwargs):
     if created:
-        Profile.objects.cerate(user = instance)
+        Profile.objects.create(user = instance)
 
 @receiver(post_save, sender = User)
-def save_profile(sender,insatnce, **kwargs):
+def save_profile(sender,instance, **kwargs):
     instance.profile.save()
