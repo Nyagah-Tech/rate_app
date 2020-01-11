4d216f136764ab401666335287cd2654b8972bbc
dan <dan@localhost.localdomain>
2020-01-11 22:30:10 +0300

    create the urls and paths to our posting newreview,new project and rating view function

--- rate_app/urls.py
+++ rate_app/urls.py
@@ -6,6 +6,10 @@ from django.conf.urls.static import static
 urlpatterns=[
     path('',views.home,name="home"),
     path('new/project/',views.post_project_view,name="post-project"),
+    path('review/project/<int:id>',views.post_review_view, name='post-review'),
+    path('review/project/post/<int:id>',views.review_post,name="review_submit"),
+    path('review/project/rate/post/<int:id>',views.post_rate_view, name='post-rate'),
+    
     
 ]
 
