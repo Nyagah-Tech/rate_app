7411b8c8c7c561f9ee0b66faea85e95baf2b318e
dan <dan@localhost.localdomain>
2020-01-12 20:09:19 +0300

    create a path that links a user with the update profile view function

--- rate_app/urls.py
+++ rate_app/urls.py
@@ -9,6 +9,7 @@ urlpatterns=[
     path('review/project/<int:id>',views.post_review_view, name='post-review'),
     path('review/project/post/<int:id>',views.review_post,name="review_submit"),
     path('review/project/rate/post/<int:id>',views.post_rate_view, name='post-rate'),
+    path('profile/user/update',views.update_profile_view,name='update-profile'),
     
     
 ]
