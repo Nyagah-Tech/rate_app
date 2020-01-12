ef4b80bb38da815382d3b098252d058e1af8e4cf
dan <dan@localhost.localdomain>
2020-01-12 20:07:18 +0300

    register the reviews and rates  to our admin

--- rate_app/admin.py
+++ rate_app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Profile,Reviews,Project_Post
+from .models import Profile,Reviews,Project_Post,Rates
 
 admin.site.register(Profile)
 admin.site.register(Project_Post)
+admin.site.register(Reviews)
+admin.site.register(Rates)
 
 # Register your models here.
